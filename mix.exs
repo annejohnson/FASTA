bf8c42617d7cca4ec26485a8c671de1c618e9915
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-12 13:00:38 -0400

    Update mix file with license, contact info, and description

--- mix.exs
+++ mix.exs
@@ -1,31 +1,31 @@
-defmodule Fasta.Mixfile do
+defmodule FASTA.Mixfile do
   use Mix.Project
 
   def project do
     [app: :fasta,
      version: "0.0.1",
      elixir: "~> 1.2",
+     description: description,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     package: package]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+    "FASTA is a tool for parsing FASTA-formatted strings in Elixir."
+  end
+
+  defp package do
+    [licenses: ["MIT"],
+     maintainers: ["annecodes@gmail.com"],
+     links: %{"GitHub" => "https://github.com/annejohnson/FASTA"}]
+  end
+
   defp deps do
     [{:parallel, "~> 0.0.3"},
      {:earmark, "~> 0.1", only: :dev},
