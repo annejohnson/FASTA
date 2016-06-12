a0b48f036010644af91031a3177588734a866642
Anne Johnson <annekjohnson23@yahoo.com>
2016-06-12 18:26:34 -0400

    Add documentation to the FASTA.valid_string?/1 function

--- lib/fasta.ex
+++ lib/fasta.ex
@@ -1,6 +1,25 @@
 defmodule FASTA do
-  @moduledoc false
+  @moduledoc """
+  Provides a function `FASTA.valid_string?/1` to indicate if a string is a valid
+  FASTA-formatted string.
+  """
 
+  @doc """
+  Returns true if a string is a valid FASTA-formatted string.
+
+  ## Parameters
+
+    - `str`: string to check for FASTA formatting
+
+  ## Example
+
+      iex> FASTA.valid_string?("> Acer rubrum
+      ...>                      GGTCGAACTGATG")
+      true
+
+      iex> FASTA.valid_string?("> Acer rubrum")
+      false
+  """
   def valid_string?(str) do
     str
     |> FASTA.Parser.parse
